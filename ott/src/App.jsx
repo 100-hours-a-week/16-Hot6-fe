fdeed973e50fe57fcba5a0b6bc5b257e5c356b94
junsik <deeptree97@gmail.com>
2025-05-02 17:08:01 +0900

    feat: 인증이 필요한 페이지 구별 및 인증 절차 추가

--- ott/src/App.jsx
+++ ott/src/App.jsx
@@ -1,9 +1,39 @@
-import { RouterProvider } from 'react-router-dom';
-import router from './routes';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RootLayout from './components/layout/RootLayout';
+import PrivateRoute from './components/PrivateRoute';
 
-function App() {
-  return <RouterProvider router={router} />;
-}
+// 페이지 컴포넌트들
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* RootLayout을 사용하는 공개 라우트 */}
+        <Route element={<RootLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+
+        {/* RootLayout을 사용하는 보호된 라우트 */}
+        <Route element={<RootLayout />}>
+          {/* <Route
+            path="/mypage"
+            element={
+              <PrivateRoute>
+                <MyPage />
+              </PrivateRoute>
+            }
+          /> */}
+        </Route>
+
+        {/* RootLayout을 사용하지 않는 라우트 */}
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
