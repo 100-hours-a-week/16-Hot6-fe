917af0c326894fcdec754145d73cd2e9c5cb8ed9
flydog98 <guinnessoverflow@gmail.com>
2025-05-12 21:39:13 +0900

    cd: yarn 사용하도록 변경

--- ott/src/config/index.js
+++ ott/src/config/index.js
@@ -3,7 +3,7 @@ const config = {
     BASE_URL: 'https://dev-backend.onthe-top.com/api/v1',
   },
   production: {
-    BASE_URL: 'https://prod-backend.onthe-top.com/api/v1', // 실제 배포 시 사용할 URL
+    BASE_URL: 'https://backend.onthe-top.com/api/v1', // 실제 배포 시 사용할 URL
   },
   test: {
     BASE_URL: 'http://test-api.onthetop.com/api/v1', // 테스트용 URL
@@ -11,5 +11,5 @@ const config = {
 };
 
 export const getConfig = () => {
-  return config[import.meta.env.MODE || 'production'];
+  return config[import.meta.env.MODE || 'development'];
 };
